26dbb5aaab0f27d34d2d30db0d2afd990a908fbf
Wayaer <wayaer@foxmail.com>
2023-02-07 10:18:38 +0800

    no message

--- lib/fl_assets_picker.dart
+++ lib/fl_assets_picker.dart
@@ -2,6 +2,8 @@ library assets_picker;
 
 export 'package:extended_image/extended_image.dart';
 export 'package:fl_video/fl_video.dart';
+export 'package:photo_manager/photo_manager.dart';
+export 'package:video_player/video_player.dart';
 export 'package:wechat_assets_picker/wechat_assets_picker.dart';
 export 'package:wechat_camera_picker/wechat_camera_picker.dart';
 
